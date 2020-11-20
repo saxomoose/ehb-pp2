0d023fe5bc32ceaf6ed25f9627cca5c97d11d2f7
maaiked <67857316+maaiked@users.noreply.github.com>
2020-11-20 10:26:56 +0100

    SVG added of InsuraQuest logo

--- insuraquest/resources/views/livewire/search-users.blade.php
+++ insuraquest/resources/views/livewire/search-users.blade.php
@@ -1,12 +1,14 @@
+
 <div>
     <input wire:model="search" type="text" placeholder="Search/Filter users..."/>
-
+<hr>
+<hr>
     <ul>
         @foreach($users as $user)
-        <br>
+        <hr>
             <li>{{ $user->name }}</li>
             <li>{{ $user->email }}</li>
-            <br>
         @endforeach
     </ul>
+
 </div>
