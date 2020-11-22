b1b3ce7afbbf1d2ad6357485d42a4024381e17eb
maaiked <67857316+maaiked@users.noreply.github.com>
2020-11-22 11:59:16 +0100

    added testaccounts for admin, user and librarian

--- insuraquest/database/seeders/DatabaseSeeder.php
+++ insuraquest/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,15 +19,31 @@ class DatabaseSeeder extends Seeder
     {
 
         DB::table('users')->insert([
-            'name' => 'gebruiker',
-            'email' => 'gebruiker@example.com',
-            'password' => '123456',
+            'name' => 'admin',
+            'email' => 'admin@insuraquest.com',
+            'password' => Hash::make('admin'),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
             'type' => 'admin',
         ]);
+        DB::table('users')->insert([
+            'name' => 'librarian',
+            'email' => 'librarian@insuraquest.com',
+            'password' => Hash::make('librarian'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'type' => 'librarian',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'user',
+            'email' => 'user@insuraquest.com',
+            'password' => Hash::make('user'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10),
+            'type' => 'user',
+        ]);
 
-         \App\Models\User::factory(45)->create();
+         \App\Models\User::factory(15)->create();
 
 
     }
