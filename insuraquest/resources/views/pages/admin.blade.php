68865d1e0424aaf74dfe36a23a81fe26bdc1f25a
maaiked <67857316+maaiked@users.noreply.github.com>
2020-11-28 14:03:22 +0100

    updated readme with smtp mailtrap

--- insuraquest/resources/views/pages/admin.blade.php
+++ insuraquest/resources/views/pages/admin.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 {{-- create compenent for admin page --}}
-
+                {{-- TO DO :: create filter for admin to quickly see guests --}}
                 @can('isAdmin', App\Models\User::class)
                 @livewire('search-users')
     <!-- The Current User Can Create Posts -->
