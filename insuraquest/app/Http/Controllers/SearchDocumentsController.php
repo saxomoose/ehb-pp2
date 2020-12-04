d1d2b3361f26ba8d1492f6ec4d4fb40a0c541423
maaiked <67857316+maaiked@users.noreply.github.com>
2020-12-04 13:23:43 +0100

    created SearchDocumentsController

---
+++ insuraquest/app/Http/Controllers/SearchDocumentsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchDocumentsController extends Controller
+{
+    //testen of we dit kunnen doen om gemakkelijker met de json response te werken?
+    //(//$collection = collect(json_decode($response));
+    //$items = $collection->where('id', 25);
+}
