59318599f3da4d28ddf34b570d7effd72c5587df
maaiked <67857316+maaiked@users.noreply.github.com>
2020-12-05 12:12:31 +0100

    search form for documents

--- insuraquest/resources/views/pages/admin.blade.php
+++ insuraquest/resources/views/pages/admin.blade.php
@@ -7,7 +7,8 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class=" p-8 bg-white overflow-hidden shadow-xl sm:rounded-lg border-8">
+                <div>
                 {{-- create compenent for admin page --}}
                 {{-- TO DO :: create filter for admin to quickly see guests --}}
                 @can('isAdmin', App\Models\User::class)
@@ -18,6 +19,7 @@
                 @cannot('isAdmin', App\Models\User::class)
                 <p>You are not authorized to view admin page</p>
                 @endcannot
+                </div>
 
             </div>
         </div>
--- insuraquest/resources/views/pages/search.blade.php
+++ insuraquest/resources/views/pages/search.blade.php
@@ -4,18 +4,94 @@
             {{ __('Search') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class=" p-8 bg-white overflow-hidden shadow-xl sm:rounded-lg border-8">
+                <div>
                 {{-- Create compenent for search page --}}
-                @can('isUser', App\Models\User::class)
-                <p>You are authorized to view search page - IN PROGRESS</p>
-    <!-- The Current User Can Create Posts -->
-                @endcan
-                @cannot('isUser', App\Models\User::class)
-                <p>You are not authorized to view search page</p>
-                @endcannot
+                    <form action="{{ route('search') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                        <div class=container>
+                            <div class="form-row">
+                                <div class="form-group col-8">
+                                    <input type="text" class="form-control" placeholder="Search for document...">
+                                </div>
+                                <div class="form-group col">
+                                    <button type="submit"
+                                    class="btn btn-success inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    Search
+                                    </button>
+                                </div>
+                            </div>
+                            <p> Select filters before quering search </p>
+
+                            <div class="form-row">
+                                <div class="form-group col-auto">
+                                    <label for="language">Language</label>
+                                    <select multiple class="form-control" name="language" size=2>
+                                        <option selected></option>
+                                        <option value="dutch">Dutch</option>
+                                        <option value="french">French</option>
+                                    </select>
+                                 </div>
+                                 <div class="form-group col-auto">
+                                    <label for="issuer">Issuer</label>
+                                    <select multiple class="form-control" name="issuer" size=2>
+                                        <option selected></option>
+                                        <option value="eu">EU</option>
+                                        <option value="be">BE</option>
+                                        <option value="vlaamsgewest">Vlaams Gewest</option>
+                                        <option value="waalsgewest">Waals Gewest</option>
+                                        <option value="brusselshoofdstedelijkgewest">Brussels Hoofdstedelijk Gewest</option>
+                                        <option value="grondwettelijkhof">Grondwettelijk hof</option>
+                                        <option value="hofvancassatie">Hof van cassatie</option>
+                                        <option value="raadvanstaat">Raad van staat</option>
+                                        <option value="hofvanberoep">Hof van beroep</option>
+                                        <option value="arbeidshof">Arbeidshof</option>
+                                        <option value="rechtbankvaneersteaanleg">Rechtbank van eerste aanleg</option>
+                                        <option value="arbeidsrechtbank">Arbeidsrechtbank</option>
+                                        <option value="ondernemingsrechtbank">Ondernemingsrechtbank</option>
+                                        <option value="politierechtbank">Politierechtbank</option>
+                                        <option value="vredegerecht">Vredegerecht</option>
+                                    </select>
+                                 </div>
+                                 <div class="form-group col-auto">
+                                    <label for="category">Category</label>
+                                    <select multiple class="form-control" name="category" size=2>
+                                        <option selected></option>
+                                        <option value="wetgeving">Wetgeving</option>
+                                        <option value="rechtspraak">Rechtspraak</option>
+                                        <option value="rechtsleer">Rechtsleer</option>
+                                    </select>
+                                 </div>
+                                 <div class="form-group col-auto">
+                                    <label for="keyword">Keyword</label>
+                                    <select multiple class="form-control"  name="keyword" size=2>
+                                        <option selected></option>
+                                        <option value="auto">Auto</option>
+                                        <option value="brand">Brand</option>
+                                        <option value="leven">Leven</option>
+                                        <option value="gezondheidszorgen">Gezondheidszorgen</option>
+                                        <option value="rechtsbijstand">Rechtsbijstand</option>
+                                        <option value="annulatie">Annulatie</option>
+                                        <option value="bijstand">Bijstand</option>
+                                        <option value="nvt">NVT</option>
+                                    </select>
+                                 </div>
+                                <div class="form-group col-auto">
+                                    <label for="date-until">Date published</label>
+                                    <input type="date" class="form-control" name="date-until">
+                                </div>
+                                <div class="form-group col-auto">
+
+                                    <label for="date-until">Date until</label>
+                                    <input type="date" class="form-control" name="date-until">
+                                </div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
             </div>
         </div>
     </div>
