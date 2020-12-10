e71d2ba5756ab83bdfb2ae243a6b3b4fbc00b7ed
maaiked <67857316+maaiked@users.noreply.github.com>
2020-12-10 16:53:21 +0100

    updated insuraquest logo on diff pages

--- insuraquest/resources/views/auth/login.blade.php
+++ insuraquest/resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            {{-- Logo insuraquest nog toe te voegen --}}
-        </x-slot>
+            <x-jet-authentication-card-logo />
+            </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
 
