51c88641d116f3c9f2a1cdef81b6ff771ed4cfa0
Olivier Thas <olivier.thas@student.ehb.be>
2020-12-11 23:21:35 +0100

    Layout query.show

--- insuraquest/resources/views/pages/query/show.blade.php
+++ insuraquest/resources/views/pages/query/show.blade.php
@@ -5,6 +5,7 @@
         </h2>
     </x-slot>
 
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
@@ -12,12 +13,13 @@
                 <div>
 
                     <div class="container mt-2 col-md-12">
-                            <h1 class="display-4">Total search results: {{ $hits }}</h1>
+                            <h1 class="display-6">Total search results: {{ $hits }}</h1>
                             @forelse ($results as $result)
                             <div class="jumbotron jumbotron-fluid">
                                 <div class="container container-fluid">
-                                    <h1 class="display-4">Doc id: {{ $result['_id'] }}</h1>
-                                    <h1 class="display-4">Score: {{ $result['_score'] }}</h1>
+                                    <h1 class="display-8">{{ $result['_source']['external']['title'] }}</h1>
+                                    <span class="d-block p-0.1 text-right">id: {{ $result['_id'] }}</span>
+                                    <span class="d-block p-0.1 text-right">score: {{ $result['_score'] }}</span>
                                     <hr class="my-4">
                                     @foreach ($result['highlight']['content'] as $highlight)
                                     <p class="lead">{!! $highlight !!}</p>
