69c7728b99ed9df23c1a2bb27b7a9846101ed6eb
maaiked <67857316+maaiked@users.noreply.github.com>
2020-12-12 16:35:37 +0100

    updated layout on searchresults

--- insuraquest/resources/views/pages/query/show.blade.php
+++ insuraquest/resources/views/pages/query/show.blade.php
@@ -1,28 +1,29 @@
 @isset($results)
-                    <div class="container mt-2 col-md-12">
-                            <h1 class="display-6">Total search results: {{ $hits }}</h1>
-                            @forelse ($results as $result)
-                            <div class="jumbotron jumbotron-fluid">
-                                <div class="container container-fluid">
-                                    <h1 class="display-8">{{ $result['_source']['external']['title'] }}</h1>
-                                    <span class="d-block p-0.1 text-right">id: {{ $result['_id'] }}</span>
-                                    <span class="d-block p-0.1 text-right">score: {{ $result['_score'] }}</span>
-                                    <hr class="my-4">
-                                    @foreach ($result['highlight']['content'] as $highlight)
-                                    <p class="lead">{!! $highlight !!}</p>
-                                    @endforeach
-                                    <div class="container col text-right">
-                                        <a class="btn btn-primary btn-sm " href="{{route('document', ['id' => $result['_id']])}}" type="submit">Read the full document</a>
-                                    </div>
-                                </div>
-                            </div>
-                            @empty
-                            <div class="jumbotron jumbotron-fluid">
-                                <div class="container container-fluid">
-                                    <p class="lead">No relevant documents for your query</p>
-                                </div>
-                            </div>
-                            @endforelse
-                    </div>
+<div class="mt-2 px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+    <h3>Total search results: {{ $hits }}</h3>
+    @forelse ($results as $result)
+    <div class="jumbotron jumbotron-fluid" style="padding: 10px">
+
+            <h5 class="display-8">{{ $result['_source']['external']['title'] }}</h5>
+            <span class="d-block p-0.1 text-right">id: {{ $result['_id'] }}</span>
+            <span class="d-block p-0.1 text-right">score: {{ $result['_score'] }}</span>
+            <hr>
+            @foreach ($result['highlight']['content'] as $highlight)
+            <p>{!! $highlight !!}</p>
+            @endforeach
+            <div class="container col text-right">
+                <a class="btn btn-primary btn-sm " href="{{route('document', ['id' => $result['_id']])}}"
+                    type="submit">Read the full document</a>
+            </div>
+
+    </div>
+    @empty
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container container-fluid">
+            <p class="lead">No relevant documents for your query</p>
+        </div>
+    </div>
+    @endforelse
+</div>
 
 @endisset
