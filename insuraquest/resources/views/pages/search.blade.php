7595a2fa85c56d175abf52bbf8adac306b164304
Olivier Thas <olivier.thas@student.ehb.be>
2020-12-29 17:36:11 +0100

    Layout search page

--- insuraquest/resources/views/pages/search.blade.php
+++ insuraquest/resources/views/pages/search.blade.php
@@ -19,97 +19,90 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script> @endpush
 
 
- <div>
+<div>
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong><br>There were some problems with your input.
+            <ul><br>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-      @if (count($errors) > 0)
-                                                <div class="alert alert-danger">
-                                                    <strong>Whoops!</strong><br> There were some problems with your input.
-                                                    <ul><br>
-                                                        @foreach ($errors->all() as $error)
-                                                        <li>{{ $error }}</li>
-                                                        @endforeach
-                                                    </ul>
-                                                </div>
-                                                @endif
+    {{print_r(Session::all())}}
+    <form action="{{ route('documentsearch') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+        <div class="form-row">
+                <div class="form-group col-auto">
+                <label for="searchtext">SEARCH FOR : </label>
+            </div>
+            <div class="form-group col-9">
+                    <input type="text" class="form-control" name="searchtext" placeholder="Search for document...">
+            </div>
+        </div>
+        <div class = "form-row">
+            <div class="form-group col-auto">
+                <label  for="excludetext">EXCLUDE FROM SEARCH : </label>
+            </div>
+            <div class="form-group col-8">
+                <input type="text" class="form-control" name="excludetext" placeholder="Words to exclude from search">
+            </div>
+        </div>
+        <p>Select filters before quering search:</p>
 
-     {{print_r(Session::all())}}
-     <form action="{{ route('documentsearch') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-                <div class="form-row">
-                     <div class="form-group col-auto">
-                        <label for="searchtext">SEARCH FOR : </label>
-                    </div>
-                    <div class="form-group col-9">
-                         <input type="text" class="form-control" name="searchtext" placeholder="Search for document...">
-                    </div>
-                </div>
-                <div class = "form-row">
-                    <div class="form-group col-auto">
-                        <label  for="excludetext">EXCLUDE FROM SEARCH : </label>
-                    </div>
-                    <div class="form-group col-8">
-                        <input type="text" class="form-control" name="excludetext" placeholder="Words to exclude from search">
-                    </div>
-                </div>
-                <p> Select filters before quering search. </p>
-
-                <div class="form-row">
-                     <div class="form-group col-auto">
-                        <label  for="language[]">Language</label>
-
-                         <select multiple="multiple"  class="selectpicker" name="language[]">
-
-                        @foreach($languages as $language )
+        <div class="form-row">
+            <div class="form-group col-auto">
+                <label  for="language[]">Language</label>
+                <select multiple="multiple"  class="selectpicker" name="language[]">
+                    @foreach($languages as $language )
                         <option value="{{ $language->name }}">{{ $language->value }}</option>
-                        @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group col-auto">
-                        <label for="issuer[]">Issuer</label>
-                        <select multiple="multiple"  class="selectpicker" name="issuer[]">
-
-                        @foreach($issuers as $issuer )
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group col-auto">
+                <label for="issuer[]">Issuer</label>
+                <select multiple="multiple"  class="selectpicker" name="issuer[]">
+                    @foreach($issuers as $issuer )
                         <option value="{{ $issuer->name }}">{{ $issuer->value }}</option>
-                        @endforeach
-                        </select>
-                     </div>
-                 </div>
-                <div class="form-row">
-                    <div class="form-group col-auto">
-                        <label for="category[]">Category</label>
-                        <select multiple="multiple"  class="selectpicker" name="category[]">
-
-                        @foreach($categories as $category )
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-auto">
+                <label for="category[]">Category</label>
+                <select multiple="multiple"  class="selectpicker" name="category[]">
+                    @foreach($categories as $category )
                         <option value="{{ $category->name }}">{{ $category->value }}</option>
-                        @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group col-auto">
-                        <label for="keyword[]">Keyword</label>
-                        <select multiple="multiple"  class="selectpicker" name="keyword[]">
-
-                        @foreach($keywords as $keyword )
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group col-auto">
+                <label for="keyword[]">Keyword</label>
+                <select multiple="multiple"  class="selectpicker" name="keyword[]">
+                    @foreach($keywords as $keyword )
                         <option value="{{ $keyword->name }}">{{ $keyword->value }}</option>
-                        @endforeach
-                        </select>
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-auto">
-                        <label for="date-from">Date published from</label>
-                        <input type="date" class="form-control" name="date-from">
-                        </div>
-                        <div class="form-group col-auto">
-                        <label for="date-until">Date until</label>
-                        <input type="date" class="form-control" name="date-until">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <button type="submit"
-                        class="btn btn-success inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        Search
-                    </button>
-                </div>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="form-row">
+            <div class="form-group col-auto">
+                <label for="date-from">Date published from</label>
+                <input type="date" class="form-control" name="date-from">
+            </div>
+            <div class="form-group col-auto">
+                <label for="date-until">Date published until</label>
+                <input type="date" class="form-control" name="date-until">
+            </div>
+        </div>
+        <div class="form-row">
+            <button type="submit" class="btn btn-success inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                Search
+            </button>
+        </div>
     </form>
 </div>
 
