242e946e46646a088a1db2128b40539eb65967ef
Olivier Thas <olivier.thas@student.ehb.be>
2020-12-29 12:31:04 +0100

    Connection Model + modified QueryController

---
+++ insuraquest/app/Models/Connection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Elasticsearch\ClientBuilder;
+
+class Connection extends Model
+{
+    use HasFactory;
+
+    public function __construct($hosts) {
+        $this->hosts = $hosts;
+    }
+
+    public static function handle() {
+        // Configure extended host for client
+        $hosts = [
+            'host' => '10.3.50.7',
+            'port' => '9200',
+            'scheme' => 'http', // other option: 'https'
+            //'user' => 'username', // relevant when using https
+            //'pass' => 'password', // relevant when using https
+        ];
+
+        $client = ClientBuilder::create() // Instantiate a new ClientBuilder
+                    ->setHosts($hosts) // Set the hosts
+                    ->build(); // Build the client object
+                    
+        return $client;
+    }
+}
